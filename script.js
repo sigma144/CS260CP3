1b1fec976b9ba30522b49cc242c5258bc65f4429
Brian Robinson <elderbrianrobinson@gmail.com>
2018-03-01 21:36:51 -0700

    Completed project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-	  questionnum: -1,
+	  questionnum: 0,
 	  score: 0,
 	  answered: false,
 	  answernum: 0,
@@ -21,7 +21,8 @@ var app = new Vue({
 		  answer3: "A3",
 		  answer4: "A4",
 		  correct: 1,
-		  answerimage: "image1",
+		  correctans: "Charles Babbage",
+		  answerimage: "image1.jpg",
 		  answertext: `Charles Babbage's 1837 Analytical Engine, equipped with memory,
 		  conditional control flow, and programmable punch cards, is widely considered to be
 		  the first programmable general-purpose computer ever designed. Sadly, due to lack of funding
@@ -30,13 +31,27 @@ var app = new Vue({
 		  original design that the first fully functional general-purpose computer would be constructed.`
 	  },
 	  {
+		  question: "This English mathematician wrote the first algorithm designed to be run on the Analytical Engine, in the process becoming the world's first programmer:",
+		  answer1: "Ada Lovelace",
+		  answer2: "A2",
+		  answer3: "A3",
+		  answer4: "A4",
+		  correct: 0,
+		  correctans: "Ada Lovelace",
+		  answerimage: "image2.jpg",
+		  answertext: `Ada Lovelace's full name is actually Augusta Ada King-Noel, Countess of Lovelace. She was encouraged by her mother to study
+		  mathematics and logic, and her friendship with Babbage led to her publication of the first computer algorithm ever made. It is believed that the
+		  Ada programming language was named in her honor.`
+	  },
+	  {
 		  question: "Born in 1912, this man's theoretical model of computation (named after him) is capable of solving any computable problem:",
 		  answer1: "A1",
 		  answer2: "Alan Turing",
 		  answer3: "A3",
 		  answer4: "A4",
 		  correct: 1,
-		  answerimage: "image2",
+		  correctans: "Alan Turing",
+		  answerimage: "image3.jpg",
 		  answertext: `Since its creation in 1936, the Turing Machine has been the key to many of
 		  computing's most landmark proofs. One such proof is that certain problems are unsolvable for
 		  even the most advanced of computers - for example, the problem of deciding with certainty whether any arbitrary
@@ -49,7 +64,8 @@ var app = new Vue({
 		  answer3: "A3",
 		  answer4: "A4",
 		  correct: 0,
-		  answerimage: "image3",
+		  correctans: "Claude Shannon",
+		  answerimage: "image4.jpg",
 		  answertext: `In addition to paving the way for modern digital computers, Shannon also worked
 as a cryptanalyst, helping the U.S. in WWII to crack the encryption algorithms of its enemies' wartime communications.
 Shannon also met Alan Turing during this time, and the two men discussed each other's ideas over tea.`
@@ -61,7 +77,8 @@ Shannon also met Alan Turing during this time, and the two men discussed each ot
 		  answer3: "William Shockley",
 		  answer4: "A4",
 		  correct: 2,
-		  answerimage: "babbage",
+		  correctans: "William Shockley",
+		  answerimage: "image5.jpg",
 		  answertext: `The transistor, a simple electronic switch which can change state billions of times a second, has been so impactful
 		  to human society that it often listed as one of the
 greatest inventions of all time, alongside discoveries like electricity and the steam engine. Modern CPU's today typically contain over a billion transistors!`
@@ -73,10 +90,66 @@ greatest inventions of all time, alongside discoveries like electricity and the 
 		  answer3: "Grace Hopper",
 		  answer4: "A4",
 		  correct: 2,
-		  answerimage: "babbage",
+		  correctans: "Grace Hopper",
+		  answerimage: "image6.jpg",
 		  answertext: `Both an incredible programmer and a loyal patriot, Hopper has become
 		  one of the most respected women in computer science, earning the nickname "Amazing Grace."
 		  Legend has it that she also popularized the term "debugging" after discovering a moth stuck inside the Navy's Harvard Mark II computer.`
+	  },
+	  {
+		  question: "This co-founder of Apple Inc. built the Apple I, Apple's first commercial desktop computer:",
+		  answer1: "A1",
+		  answer2: "A2",
+		  answer3: "A3",
+		  answer4: "Steve Wozniak",
+		  correct: 3,
+		  correctans: "Steve Wozniak",
+		  answerimage: "image7.jpg",
+		  answertext: `What's magnificent about the Apple I is that it was the work of only one man: Steve Wozniak.
+		  The computer originally came without a case, so inventive owners came up with a variety of custom-built cases.
+		  Today, only six working Apple I's are confirmed to exist. Who knows what the PC scene would be like today without
+		  this milestone?`
+	  },
+	  {
+		  question: "This Canadian computer scientist was the founder of the Java programming language:",
+		  answer1: "A1",
+		  answer2: "A2",
+		  answer3: "A3",
+		  answer4: "James Gosling",
+		  correct: 3,
+		  correctans: "James Gosling",
+		  answerimage: "image8.jpg",
+		  answertext: `Unlike other computing greats such as Bill Gates and Steve Jobs who never finished college, James Gosling
+		  earned a Ph.D. at Carnegie Mellon university and went on to join Sun Microsystems, which developed the Java language.
+		  Although Gosling would leave Sun Microsystems in 2010 after the company was acquired by Oracle, it is hard to doubt Gosling's
+		  impact on the industry. According to Oracle, over 3 billion devices today run Java.`
+	  },
+	  {
+		  question: "Born in South Africa, this man taught himself programming at the age of 12, and is famous today partly for his wild invention ideas such as the Hyperloop:",
+		  answer1: "A1",
+		  answer2: "A2",
+		  answer3: "A3",
+		  answer4: "Elon Musk",
+		  correct: 3,
+		  correctans: "Elon Musk",
+		  answerimage: "image9.jpg",
+		  answertext: `In addition to his scientific innovations, Elon Musk made another contribution to computer science in 2015 with the founding of OpenAI,
+		  a non-profit focused on AI development and research. OpenAI, Google, and other companies have successfully developed neural networks capable of
+		  beating even the smartest humans at complex strategy games, including Chess, Go, and Dota 2. AI is expected to become more and more ubiquitous over the next few decades,
+		  impacting many fields currently dominated by humans.`
+	  },
+	  {
+		  question: "This man received the 2016 Turing Award for his invention of the World Wide Web and the first web browser:",
+		  answer1: "Tim Berners-Lee",
+		  answer2: "A2",
+		  answer3: "A3",
+		  answer4: "A4",
+		  correct: 0,
+		  correctans: "Tim Berners-Lee",
+		  answerimage: "image10.jpg",
+		  answertext: `Though a rudimentary form of the "internet" existed at the time Tim Berners-Lee was born, his creation of the HTTP protocol, HTML
+		  markup language, and web browser made the World Wide Web - what we now think of as the Internet - possible. Tim Berners-Lee is currently 62
+		  years old and works as director of the World Wide Web Consortium (W3C), an organization in change of the standards and protocols keeping the Internet functioning today.`
 	  },
 	  
 	  ],
@@ -95,7 +168,7 @@ greatest inventions of all time, alongside discoveries like electricity and the 
 		  if (app.answered)
 		  {
                           app.answerstyle[app.answernum] = "background-color: #ffbbbb; border: 2px solid red;";
-			  app.answerstyle[app.curquestion.correct] = "background-color: #ddffdd; border: 2px solid green;";
+			  app.answerstyle[app.curquestion.correct] = "background-color: #bbffbb; border: 2px solid green;";
                           if (app.answernum === app.curquestion.correct)
                               app.score++;
 		  }
@@ -112,6 +185,8 @@ greatest inventions of all time, alongside discoveries like electricity and the 
           begin: function() {
                   app.state = 'quiz';
                   app.questionnum = 0;
+				  app.score = 0;
+				  app.answernum = 0;
                   app.curquestion = app.quiz[0];
           },
 	  answer1: function() {
@@ -131,17 +206,18 @@ greatest inventions of all time, alongside discoveries like electricity and the 
 		  app.answernum = 3;
 	  },
 	  next: function() {
-                  app.answered = false;
-                  app.answernum = 0;
+          app.answered = false;
+          app.answernum = 0;
 		  app.questionnum++;
-                  if (app.questionnum >= app.quiz.length)
-                  {
-                        app.state = 'results'
-                  }
-                  else
-                  {
-                        app.curquestion = app.quiz[app.questionnum];
-                  }
+		  window.scrollTo(0, 0);
+          if (app.questionnum >= app.quiz.length)
+          {
+              app.state = 'results'
+          }
+          else
+          {
+              app.curquestion = app.quiz[app.questionnum];
+          }
 	  }
   }
 });
